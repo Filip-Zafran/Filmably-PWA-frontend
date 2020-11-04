d86b76989f1c7c50a591cde2ddb9d019202270f3
FichoZee <filip.zafran@gmail.com>
2020-11-04 15:58:27 +0100

    implement login/logout Pages, Dashboard

--- src/App.js
+++ src/App.js
@@ -1,74 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
-import Logout from '../src/Pages/Logout';
-import Dashboard from '../src/Pages/Dashboard';
-import Movies from '../src/Pages/Movies';
-import Friends from '../src/Pages/Friends';
-import MatchPage from '../src/Pages/MatchPage';
+import Dashboard from './Pages/Dashboard';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 
+import Login from './Pages/Login';
+
+// import { Route, BrowserRouter as Router, Link, Switch } from 'react-router-dom';
+// import Dashboard from '../src/Pages/Dashboard';
+
+// one has to set up the input field in React through destructuring and useState Hook
 const App = () => {
-	return <Headline />;
-};
-
-const Headline = () => {
-	const [ loginInput, setLoginInput ] = useState(' your.email@here.de');
-	const [ passwordInput, setPasswordInput ] = useState(' enter pass');
+	// if we are logged in we render Dashboard and other components
+	// if is loggedin then state is false and only renders loginpage
 
 	return (
-		<div className="App">
-			<Router>
-				<h1>NAVBAR</h1>
-				<br />
-				<nav>
-					<ul>
-						<li>
-							<Link to="/Logout">Logout</Link>
-						</li>
-					</ul>
-					<ul>
-						<li>
-							<Link to="/Dashboard">Dashboard</Link>
-						</li>
-					</ul>
-					<ul>
-						<li>
-							<Link to="/Movies">My Movies</Link>
-						</li>
-					</ul>
-					<ul>
-						<li>
-							<Link to="/Friends">Friends</Link>
-						</li>
-					</ul>
-					<ul>
-						<li>
-							<Link to="/MatchPage">GO!</Link>
-						</li>
-					</ul>
-				</nav>
-				<Switch>
-					<Route path="/Logout" component={Logout} />
-					<Route path="/Dashboard" component={Dashboard} />
-					<Route path="/Movies" component={Movies} />
-					<Route path="/Friends" component={Friends} />
-					<Route path="/MatchPage" component={MatchPage} />
-				</Switch>
-			</Router>
-			<br />
-			<br /> <br /> <br />
-			<h1>Login Page</h1>
-			<br />
-			<label> user name </label>
-			<input type="text" value={loginInput} onChange={(event) => setLoginInput(event.target.value)} />
-			<br />
-			<br />
-			<label> password </label>
-			<input type="text" value={passwordInput} onChange={(event) => setPasswordInput(event.target.value)} />
-			<br />
-			<br />
-			<button> LOG IN </button>
-		</div>
+		<Router>
+			<Switch>
+				<PrivateRoute path="/dashboard">
+					<Dashboard />
+				</PrivateRoute>
+
+				<Route path="/">
+					<Login />
+				</Route>
+			</Switch>
+		</Router>
 	);
 };
 
