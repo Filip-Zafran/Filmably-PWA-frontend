b79a5bc8b5b3fac86d719f74063e076e7c2ec41c
Edith <edith.chevallier@hotmail.fr>
2020-11-05 15:08:17 +0100

    APP=> changes the routes, Welcome = Landing Page, Register page with route to backend

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Welcome from './Pages/Welcome.js'
+
+//define a variable element that will hold the different routes to be taken based on if logged in or not
+let elem;
+// identify what is the current path and serve the appropriate component. Welcome is the not logged component
+if (window.location.pathname === "/Welcome") {
+	elem = <Welcome />
+} else {
+	//init(store) socket will only work if user is logged
+	// serve app if user is loggedin 
+	// <Provider store={store}>
+	elem = <App />
+	//    </Provider >
+}
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	// <React.StrictMode>
+	// 	<App />
+	// </React.StrictMode>,
+	elem,
 	document.getElementById('root')
 );
 
