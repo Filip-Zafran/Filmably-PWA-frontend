ac339af53ea80c30c2090ea2f8f5483b989dd16c
FichoZee <filip.zafran@gmail.com>
2020-11-05 11:33:24 +0100

    clear comments

--- src/App.js
+++ src/App.js
@@ -3,17 +3,9 @@ import './App.css';
 import Dashboard from './Pages/Dashboard';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
-
 import Login from './Pages/Login';
 
-// import { Route, BrowserRouter as Router, Link, Switch } from 'react-router-dom';
-// import Dashboard from '../src/Pages/Dashboard';
-
-// one has to set up the input field in React through destructuring and useState Hook
 const App = () => {
-	// if we are logged in we render Dashboard and other components
-	// if is loggedin then state is false and only renders loginpage
-
 	return (
 		<Router>
 			<Switch>
@@ -30,6 +22,3 @@ const App = () => {
 };
 
 export default App;
-
-// add input in React
-// https://www.robinwieruch.de/react-function-component
