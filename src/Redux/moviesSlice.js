77ede93ffd6b048a2f5a2cb30bd5648a1058b521
HelenHMaher <helenharlanmaher@gmail.com>
2020-11-06 17:27:10 +0100

    added notes

--- src/Redux/moviesSlice.js
+++ src/Redux/moviesSlice.js
@@ -7,11 +7,14 @@ import axios from "axios";
 
 const moviesAdapter = createEntityAdapter();
 
+//initial state of moviesSlice
 const initialState = moviesAdapter.getInitialState({
   status: "idle",
   error: null,
 });
 
+//async thunk that will fetch the top 250 movies list from IMDB and store them
+//in the "movies" section of the redux state
 export const fetchMovies = createAsyncThunk("movies/fetchMovies", async () => {
   const response = await axios.get(
     `https://imdb-api.com/en/API/Top250Movies/${process.env.REACT_APP_IMDB_KEY}`
@@ -19,6 +22,7 @@ export const fetchMovies = createAsyncThunk("movies/fetchMovies", async () => {
   return response;
 });
 
+//creates moviesSlice when fetchMovies is fullfilled it will populate the movies slice
 const moviesSlice = createSlice({
   name: "movies",
   initialState,
@@ -30,5 +34,7 @@ const moviesSlice = createSlice({
 
 export default moviesSlice.reducer;
 
+//selects all moves from the redux store
+//Note: the shape of the redux state is a bit awkward but I'm not sure how to clean this up
 export const selectAllMovies = (state) =>
   state.movies ? state.movies.entities.undefined.items : [];
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesReducer from "./moviesSlice";
 
+//creates a redux store with defined slices and imports the reducers for each
+
 export default configureStore({
   reducer: {
     user: userReducer,
