87c06712e6dca3203b38c94eb07e4ddc4deaac28
Edith <edith.chevallier@hotmail.fr>
2020-11-07 20:04:38 +0100

    solved issue about emptying input field on 2nd if statement, using key attribute

--- src/Pages/Resetpw.js
+++ src/Pages/Resetpw.js
@@ -4,18 +4,20 @@ import Login from './Login';
 import { Link } from 'react-router-dom'
 
 
-export default function Resetpw() {
+export default function Resetpw(e) {
 
-    // console.log('Iam a resetpw bitch')
+    console.log('Iam a resetpw bitch')
 
     //bugs on the 2Nd display about the prefilled input
     // bug on the third display no showing up
 
     const [values, setValue] = useState('');
+    const [code, setCode] = useState('');
     const [error, setError] = useState();
     const [step, setSteps] = useState(0)
 
     const submitEmail = () => {
+
         setSteps({
             step: 1
         })
@@ -41,6 +43,9 @@ export default function Resetpw() {
         //     })
 
         // return [error, submitEmail()]
+        setValue({
+            values: ''
+        })
     }
 
     const submitCode = (e) => {
@@ -49,6 +54,8 @@ export default function Resetpw() {
         setSteps({
             step: 2
         })
+
+        return []
 
     }
 
@@ -59,24 +66,41 @@ export default function Resetpw() {
             ...values,
             [e.target.name]: e.target.value
         })
+
+
         return [values, setValue()]
     }
+
+
+    // const handleCodeChange = (e) => {
+    //     e.preventDefault()
+    //     setCode({
+    //         ...code,
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
+
+
 
     const getCurrentDisplay = () => {
         console.log("step in ftecurrentdisplay", step)
         if (step == 0) {
             return (
                 <React.Fragment >
-                    <input type='email' name='email' placeholder='Please enter your email' onInput={e => setValue(e.target.value)} onChange={handleChange} />
-                    <button onClick={submitEmail}> Next </button >
+                    <form>
+                        <input id="mail" input key="email" type='email' name='email' placeholder='Please enter your email' onInput={e => setValue(e.target.value)} onChange={handleChange} />
+                        <button onClick={submitEmail}> Next </button >
+                    </form>
                 </React.Fragment>
             )
         } else if ({ step: 1 }) {
             return (
                 <React.Fragment>
-                    <input type='password' name='code' placeholder='Please enter the code you received' onInput={e => setValue(e.target.value)} onChange={handleChange} />
-                    <input type='password' name='Newcode' placeholder='Please enter your new PW' onInput={e => setValue(e.target.value)} onChange={handleChange} />
-                    <button onClick={submitCode}> Next </button >
+                    <form>
+                        <input id="code" key="secret-code" type='password' name='code' placeholder='Please enter the code you received' onInput={e => setValue(e.target.value)} onChange={handleChange} />
+                        <input type='password' name='Newcode' placeholder='Please enter your new PW' onInput={e => setValue(e.target.value)} onChange={handleChange} />
+                        <button onClick={submitCode}> Next </button >
+                    </form>
                 </React.Fragment>
             )
         } else if ({ step: 2 }) {
