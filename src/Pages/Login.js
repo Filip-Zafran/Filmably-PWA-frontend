4d8a4dca704fcae5df1c0d890a55fd1070c0d759
Edith <edith.chevallier@hotmail.fr>
2020-11-09 12:03:52 +0100

    took Login from Authentication

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import Axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Login() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-	const history = useHistory()
+	const history = useHistory();
 
+	//this could be changed to an async function
+	//this verification route will work if you run the server from index.js on your local machine
+	//currently the credentials username:Admin password:password will work
+	//a registration route is set up under POST "http://localhost:5000/authentication/register" (also accepts username and pw)
 	function login() {
-		//need to send data to back end for verification that there is match btw what has been entered and what is stored
-		localStorage.setItem("isAuthenticated", true)
-		history.replace("/dashboard")
+		Axios({
+			method: "POST",
+			data: {
+				username: username,
+				password: password,
+			},
+			withCredentials: true,
+			url: "http://localhost:5000/authenticate/login",
+		}).then((res) => {
+			if (res.data.message === "Successfully Authenticated") {
+				setUsername("");
+				setPassword("");
+				localStorage.setItem("isAuthenticated", true);
+				history.replace("/dashboard");
+			}
+		});
 	}
 
-
 	return (
-		<>
-			<br />
-			<br /> <br /> <br />
+		<div className="login">
 			<h1>Login Page</h1>
-			<br />
 			<label> user name </label>
-			<input type="text" />
-			<br />
-			<br />
+			<input
+				type="text"
+				placeholder="username"
+				onChange={(e) => setUsername(e.target.value)}
+				value={username}
+			/>
 			<label> password </label>
-			<input type="text" />
-			<br />
-			<br />
-			<button onClick={login}> LOG IN </button>
-		</>
+			<input
+				type="password"
+				onChange={(e) => setPassword(e.target.value)}
+				value={password}
+			/>
+			<button onClick={login}> Login </button>
+		</div>
 	);
 }
