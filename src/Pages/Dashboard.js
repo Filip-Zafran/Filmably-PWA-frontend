276f91c4e835f0a0cceff7cd138ad8b4911aa764
FichoZee <filip.zafran@gmail.com>
2020-11-11 12:07:02 +0100

    Resolve conflicts in Dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,62 +1,20 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Route, BrowserRouter as Router, Switch, Link, useHistory } from 'react-router-dom';
-import { ProfilePage } from '../components/Profile';
+import React, { useEffect } from 'react';
+import { Route, BrowserRouter as Router, Switch, useHistory, Link } from 'react-router-dom';
+import Movies from './Movies';
 import Friends from './Friends';
 import MatchPage from './MatchPage';
-=======
-import React, { useEffect } from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  useHistory,
-  Link,
-} from "react-router-dom";
-import Movies from "./Movies";
-import Friends from "./Friends";
-import MatchPage from "./MatchPage";
+import { ProfilePage } from '../components/Profile';
 
-import "./Dashboard.css";
->>>>>>> main
+import './Dashboard.css';
 
 export default function Dashboard() {
-  const history = useHistory();
+	const history = useHistory();
 
-  function logout() {
-    localStorage.removeItem("isAuthenticated");
-    history.replace("/");
-  }
+	function logout() {
+		localStorage.removeItem('isAuthenticated');
+		history.replace('/');
+	}
 
-  return (
-    <div>
-      <Router>
-        <br />
-        <p> Dashboard </p>
-        <nav>
-          <ul>
-            <li>
-              <button onClick={logout}>Logout</button>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <Link to="/Movies">My Movies</Link>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <Link to="/Friends">Friends</Link>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <Link to="/MatchPage">GO!</Link>
-            </li>
-          </ul>
-        </nav>
-
-<<<<<<< HEAD
 	return (
 		<div>
 			<Router>
@@ -92,14 +50,4 @@ export default function Dashboard() {
 			</Router>
 		</div>
 	);
-=======
-        <Switch>
-          <Route path="/Movies" component={Movies} />
-          <Route path="/Friends" component={Friends} />
-          <Route path="/MatchPage" component={MatchPage} />
-        </Switch>
-      </Router>
-    </div>
-  );
->>>>>>> main
 }
