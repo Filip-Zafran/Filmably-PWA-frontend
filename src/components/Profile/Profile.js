c74d199fb17498918e62fb613811cac9f2ce1626
FichoZee <filip.zafran@gmail.com>
2020-11-11 23:55:55 +0100

    import icons and avatar, start with profile css

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,58 +1,64 @@
 import React from 'react';
 // import Dropdown from 'react-dropdown';
 // import 'react-dropdown/style.css'
-import OnClickTextEdit from './OnClickTextEdit'
-
-
+import OnClickTextEdit from './OnClickTextEdit';
+import { BottomNav } from '../BottomNav';
+import './Profile.css';
+import gearIcon from './gearIcon';
+import uploadeIcon from './uploadIcon';
 
 export function Profile() {
-
-
 	// const [ modal, setModal ] = useState(false);
 	// const toggle = () => setModal(!modal);
 
-const options = [
-        'newb ( < 18)', 
-        'barely legal (18 - 21)', 
-        'generation z (22 - 30)',
-        'midlife crisis (30 - 40)', 
-        'golden age (40 - 50)',
-        'old yeller (50+)'
-  ];
-const defaultOption = options[0]
+	const options = [
+		'newb ( < 18)',
+		'barely legal (18 - 21)',
+		'generation z (22 - 30)',
+		'midlife crisis (30 - 40)',
+		'golden age (40 - 50)',
+		'old yeller (50+)'
+	];
+	const defaultOption = options[0];
 
-  return (
-  <>
-                       
-			<br />
-			<br /> <br /> <br />
-            <h1> Hello</h1><OnClickTextEdit /> 
-      <br />
-      <h1>UPLOADER COMES HERE</h1>
-             <img 
-                src="https://i.otto.de/i/otto/26744698?h=520&w=551&sm=clamp"
-                alt="new"
-                style={{height: "250px"}}
-      />
-	<br />
-      <label> First Name:</label><OnClickTextEdit /> 
-      <br />
-      <label> Last Name:</label><OnClickTextEdit />
-      <br />
-      <label> Short Bio</label><OnClickTextEdit /> 
-      <br />
-      <label> e-mail:</label><OnClickTextEdit /> 
-      <br />
-      <label> Age Range: DROPDOWN   </label>
-      {/* <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select your age range" /> */}
+	return (
+		<div className="profile__container">
+			<div profile__header-container>
+				<gearIcon />
+				<img className="profile__profile-picture" src="https://shmector.com/_ph/4/270476539.png" alt="new" />
+				<uploadeIcon />
+			</div>
+			<div className="profile__name-box">
+				<h2>Linda Bear</h2>
+				<h3>26 matches ★ 103 likes </h3>
+				<h4>“It’s only after we’ve lost everything that we’re free to do anything.”</h4>
+			</div>
 
-          {/* <br />
-      <label> Fav movie:</label><OnClickTextEdit /> 
-      <br /> */}
+			<div className="profile__info-container">
+				<label> First Name:</label>
+				<OnClickTextEdit />
 
-  
+				<label> Last Name:</label>
+				<OnClickTextEdit />
 
+				<label> Short Bio</label>
+				<OnClickTextEdit />
 
-            </>
-    )
+				<label> e-mail:</label>
+				<OnClickTextEdit />
+
+				<label> Age Range: DROPDOWN </label>
+				{/* <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select your age range" /> */}
+			</div>
+
+			<div className="profile__likes-container">
+				<div className="profile__text-container">
+					<p>Likes</p>
+					<p>Show All ></p>
+				</div>
+			</div>
+
+			<BottomNav />
+		</div>
+	);
 }
