b668a70b1b49cf2f4ed17407444266d5b37f823c
FichoZee <filip.zafran@gmail.com>
2020-11-12 20:28:53 +0100

    change to single quotes in index.html, apps.css, index.ks

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Welcome from './Pages/Welcome.js';
-// import { useLocation } from "react-router-dom";
+// import { useLocation } from 'react-router-dom';
 
 //define a variable element that will hold the different routes to be taken based on if logged in or not
 let elem;
@@ -17,26 +17,26 @@ let elem;
 
 // identify what is the current path and serve the appropriate component. Welcome is the not logged component
 if (window.location.pathname === '/Welcome') {
-  // console.log("window", window)
-  elem = <Welcome />;
+	// console.log('window', window)
+	elem = <Welcome />;
 } else {
-  //init(store) socket will only work if user is logged
-  // serve app if user is loggedin
-  elem = (
-    //the ENTIRE app MUST be mounted WITHIN the Redux provider
-    //Otherwise the Redux store will not be consistantly accessable
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+	//init(store) socket will only work if user is logged
+	// serve app if user is loggedin
+	elem = (
+		//the ENTIRE app MUST be mounted WITHIN the Redux provider
+		//Otherwise the Redux store will not be consistantly accessable
+		<Provider store={store}>
+			<App />
+		</Provider>
+	);
 }
 
 ReactDOM.render(
-  // <React.StrictMode>
-  // 	<App />
-  // </React.StrictMode>,
-  elem,
-  document.getElementById('root')
+	// <React.StrictMode>
+	// 	<App />
+	// </React.StrictMode>,
+	elem,
+	document.getElementById('root')
 );
 
 reportWebVitals();
