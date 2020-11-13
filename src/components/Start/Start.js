1dfde5456b12575c2d3246f6f4be6ca01d63aabd
HelenHMaher <helenharlanmaher@gmail.com>
2020-11-13 17:07:30 +0100

    movieQuotes

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -3,10 +3,18 @@ import { Link } from 'react-router-dom';
 import './Start.css';
 import { CirclesBackground } from '../../components/styleElements/CirclesBackground';
 import { LetsStartButton } from '../../components/styleElements/buttons/LetsStartButton';
+import { movieQuotes } from './movieQuotes';
 
 export const Start = () => {
+  const quote = movieQuotes[Math.floor(Math.random() * movieQuotes.length)][0];
+
   return (
     <div className="start">
+      <div className="start__textBox">
+        <div className="start__quote">{quote}</div>
+        <div className="start__space"></div>
+        <div className="start__noDisplay">{quote}</div>
+      </div>
       <Link to="/MatchPage">
         <LetsStartButton label="Let's start" />
       </Link>
