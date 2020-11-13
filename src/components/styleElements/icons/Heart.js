520b9212c48a5063ef433633d9360971dce91753
HelenHMaher <helenharlanmaher@gmail.com>
2020-11-13 11:50:33 +0100

    icons

--- src/components/styleElements/icons/Heart.js
+++ src/components/styleElements/icons/Heart.js
@@ -1,19 +1,21 @@
-import React from "react";
-import "./Icon.css";
+import React from 'react';
+import './Icon.css';
 
 //active is either true or false
 
-export function Heart({ active }) {
+export function Heart({ active, size }) {
   return (
     <svg
-      className="iconSvg"
+      width={size ? size : null}
+      height={size ? size : null}
+      className={size ? null : 'iconSvg'}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M19.612 5.41452C19.1722 4.96607 18.65 4.61034 18.0752 4.36763C17.5005 4.12492 16.8844 4 16.2623 4C15.6401 4 15.0241 4.12492 14.4493 4.36763C13.8746 4.61034 13.3524 4.96607 12.9126 5.41452L11.9998 6.34476L11.087 5.41452C10.1986 4.50912 8.99364 4.00047 7.73725 4.00047C6.48085 4.00047 5.27591 4.50912 4.38751 5.41452C3.4991 6.31992 3 7.5479 3 8.82833C3 10.1088 3.4991 11.3367 4.38751 12.2421L5.30029 13.1724L11.9998 20L18.6992 13.1724L19.612 12.2421C20.0521 11.7939 20.4011 11.2617 20.6393 10.676C20.8774 10.0902 21 9.46237 21 8.82833C21 8.19428 20.8774 7.56645 20.6393 6.9807C20.4011 6.39494 20.0521 5.86275 19.612 5.41452V5.41452Z"
-        className={active ? "activeIcon" : "inactiveIcon"}
+        className={active ? 'activeIcon' : 'inactiveIcon'}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
--- src/components/styleElements/icons/Thunder.js
+++ src/components/styleElements/icons/Thunder.js
@@ -1,19 +1,21 @@
-import React from "react";
-import "./Icon.css";
+import React from 'react';
+import './Icon.css';
 
 //active is true or false
 
-export function Thunder({ active }) {
+export function Thunder({ active, size }) {
   return (
     <svg
-      className="iconSvg"
+      width={size ? size : null}
+      height={size ? size : null}
+      className={size ? null : 'iconSvg'}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M12.8889 3L4 13.8H12L11.1111 21L20 10.2H12L12.8889 3Z"
-        className={active ? "activeIcon" : "inactiveIcon"}
+        className={active ? 'activeIcon' : 'inactiveIcon'}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
