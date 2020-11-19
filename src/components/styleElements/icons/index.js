be57049d2a5b5881d425063e2d456874b057a4d8
HelenHMaher <helenharlanmaher@gmail.com>
2020-11-19 14:55:24 +0100

    align with figma

--- src/components/styleElements/icons/index.js
+++ src/components/styleElements/icons/index.js
@@ -4,22 +4,26 @@
 export { Thunder } from './Thunder';
 export { Heart } from './Heart';
 export { Star } from './Star';
+
+//To change default size: accepts "size" prop (NUMBER)
+//square icons so size is used as both width and height
+//accepts string "className" to select a className that will determine stroke color
 export { Flip } from './Flip';
 
 //To change default size: accepts "size" prop (NUMBER)
 //square icons so size is used as both width and height
 //accepts boolean "gold" to change color to gold
-export { LogoActive } from "./LogoActive";
+export { LogoActive } from './LogoActive';
 
 //To change default size: accepts "size" prop (NUMBER)
 //square icons so size is used as both width and height
-export { Logo } from "./Logo";
-export { X } from "./X";
-export { Settings } from "./Settings";
-export { ChevronRight } from "./ChevronRight";
-export { ChevronLeft } from "./ChevronLeft";
-export { Download } from "./Download";
+export { Logo } from './Logo';
+export { X } from './X';
+export { Settings } from './Settings';
+export { ChevronRight } from './ChevronRight';
+export { ChevronLeft } from './ChevronLeft';
+export { Download } from './Download';
 
 //IS NOT A SQUARE ICON
 //To change default size: accepts "width" and "height" props (NUMBER)
-export { Check } from "./Check";
+export { Check } from './Check';
