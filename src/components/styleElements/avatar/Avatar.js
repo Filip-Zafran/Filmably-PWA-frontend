bd972cdb5f05b0221d34fad78d375d76c866bd21
baskle <baaklee@gmail.com>
2020-11-20 20:52:46 +0100

    created avatar component

---
+++ src/components/styleElements/avatar/Avatar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./avatar.css";
+
+const Avatar = () => {
+  return (
+    <div className="avatarBackground">
+      <div className="cirleSmall"></div>
+      <div className="cirleMedium"></div>
+
+      <div className="initials">
+        <h3>LM</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
