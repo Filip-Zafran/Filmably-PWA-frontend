ab7e50b6834e7b77944a440de56f909bddf9be80
HelenHMaher <helenharlanmaher@gmail.com>
2020-11-23 17:51:56 +0100

    toggle colors

--- src/components/FilterPage/FilterGroup.js
+++ src/components/FilterPage/FilterGroup.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './FilterGroup.css';
 
-export const FilterGroup = ({ name, filters }) => {
-  const options = filters.map((filter) => (
-    <div className="filterGroup__option" key={filter}>
-      {filter}
-    </div>
-  ));
+export const FilterGroup = ({ name, filters, clickHandler }) => {
+  const keys = Object.keys(filters);
+
+  const options = keys.map((key) => {
+    const toggleActive = () => {
+      clickHandler(key);
+    };
+    return (
+      <div
+        onClick={toggleActive}
+        className={
+          filters[key] ? 'filterGroup__optionActive' : 'filterGroup__option'
+        }
+        key={key}
+      >
+        {key}
+      </div>
+    );
+  });
 
   return (
     <div className="filterGroup">
--- src/components/FilterPage/FilterPage.js
+++ src/components/FilterPage/FilterPage.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FilterGroup } from './FilterGroup';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import './FilterPage.css';
 
 export const FilterPage = ({ toggle, seeFilters, hidden }) => {
-  const timeFilters = ['1990s', '1980s', 'New Releases'];
-  const genreFilters = [
-    'Art',
-    'Western',
-    'Romance',
-    'Science Fiction',
-    'Drama',
-    'Action',
-    'Comedy',
-    'Horror',
-    'Top Rated',
-  ];
+  const [timeFilters, setTimeFilters] = useState({
+    '1990s': false,
+    '1980s': false,
+    'New Releases': false,
+  });
+  const [genreFilters, setGenreFilters] = useState({
+    Art: false,
+    Western: false,
+    Romance: false,
+    'Science Fiction': false,
+    Drama: false,
+    Action: false,
+    Comedy: false,
+    Horror: false,
+    'Top Rated': false,
+  });
+
+  const toggleActive = (getter, setter) => (filter) =>
+    setter({ ...getter, [filter]: !getter[filter] });
+
+  const clickOkay = () => {
+    //save changes
+    toggle();
+  };
+
+  const clickCancel = () => {
+    //reinitialize default values
+    toggle();
+  };
 
   return (
     <div
@@ -31,15 +48,23 @@ export const FilterPage = ({ toggle, seeFilters, hidden }) => {
             Let's Start
           </motion.div>
           <motion.div layout className="letsStart__content">
-            <FilterGroup name="Time" filters={timeFilters} />
-            <FilterGroup name="Genre" filters={genreFilters} />
+            <FilterGroup
+              clickHandler={toggleActive(timeFilters, setTimeFilters)}
+              name="Time"
+              filters={timeFilters}
+            />
+            <FilterGroup
+              clickHandler={toggleActive(genreFilters, setGenreFilters)}
+              name="Genre"
+              filters={genreFilters}
+            />
           </motion.div>
           <motion.div layout className="filterPage__buttons">
-            <div onClick={toggle} className="filterPage__cancel">
+            <div onClick={clickCancel} className="filterPage__cancel">
               Cancel
             </div>
             <Link to="/matchPage">
-              <div onClick={toggle} className="filterPage__ok">
+              <div onClick={clickOkay} className="filterPage__ok">
                 OK
               </div>
             </Link>
