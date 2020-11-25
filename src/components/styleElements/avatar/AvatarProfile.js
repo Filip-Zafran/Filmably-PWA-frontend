acc5b9f03ec474251d11499e64f8d685f96f41d7
baskle <baaklee@gmail.com>
2020-11-25 19:19:14 +0100

    fixed avatar overflow issue

---
+++ src/components/styleElements/avatar/AvatarProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./avatar.css";
+import { AvatarCircles } from "./AvatarCircles";
+
+const AvatarProfile = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default AvatarProfile;
