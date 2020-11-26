7ae04029036f47dcf6fe079d486fd84d2dc17ca1
HelenHMaher <helenharlanmaher@gmail.com>
2020-11-26 22:33:10 +0100

    filters saved as strings

--- src/components/FilterPage/FilterPage.js
+++ src/components/FilterPage/FilterPage.js
@@ -6,31 +6,61 @@ import axios from 'axios';
 import './FilterPage.css';
 
 export const FilterPage = ({ toggle, seeFilters, hidden }) => {
-  const [timeFilters, setTimeFilters] = useState({
-    '1990s': false,
-    '1980s': false,
-    'New Releases': false,
-  });
-  const [genreFilters, setGenreFilters] = useState({
-    Art: false,
-    Western: false,
-    Romance: false,
-    'Science Fiction': false,
-    Drama: false,
-    Action: false,
-    Comedy: false,
-    Horror: false,
-    'Top Rated': false,
-  });
+  const [timeFilters, setTimeFilters] = useState({});
+  const [genreFilters, setGenreFilters] = useState({});
+
+  const currentTimeFilters = [
+    '2020s',
+    '2010s',
+    '2000s',
+    '1990s',
+    '1980s',
+    '1970s',
+  ];
+  const currentGenreFilters = [
+    'Western',
+    'Romance',
+    'Science Fiction',
+    'Drama',
+    'Action',
+    'Comedy',
+    'Horror',
+  ];
 
   const history = useHistory();
   const location = useLocation();
 
   const serverURL = process.env.REACT_APP_SERVER;
 
+  //toggles the filters from active to inactive in the state
   const toggleActive = (getter, setter) => (filter) =>
     setter({ ...getter, [filter]: !getter[filter] });
 
+  //compares possible filters with active filters from DB to get the current state
+  const mapOver = (activeFilters, possibleFilters) => {
+    const addKeys = (currentFilters) => {
+      for (let i = 0; i < possibleFilters.length; i++) {
+        const index = activeFilters.indexOf(possibleFilters[i]);
+        index > -1
+          ? (currentFilters[possibleFilters[i]] = true)
+          : (currentFilters[possibleFilters[i]] = false);
+      }
+      return currentFilters;
+    };
+    const result = addKeys({});
+    return result;
+  };
+
+  //takes the current state and makes an array of active filters
+  const reverseMapOver = (filterObject) => {
+    let filterArray = [];
+    for (let [key, value] of Object.entries(filterObject)) {
+      if (value) filterArray.push(key);
+    }
+    return filterArray;
+  };
+
+  //gets the current filters and converts them into an object in the state
   const getFilters = async () => {
     try {
       const response = await axios({
@@ -38,10 +68,21 @@ export const FilterPage = ({ toggle, seeFilters, hidden }) => {
         withCredentials: true,
         url: `${serverURL}/likeTracker/filters`,
       });
-      console.log(response.data);
       if (Object.keys(response.data.filters).length > 0) {
-        setGenreFilters(response.data.filters.genreFilters);
-        setTimeFilters(response.data.filters.timeFilters);
+        const gFilters = mapOver(
+          response.data.filters.genreFilters,
+          currentGenreFilters
+        );
+        const tFilters = mapOver(
+          response.data.filters.timeFilters,
+          currentTimeFilters
+        );
+        setGenreFilters(gFilters);
+        setTimeFilters(tFilters);
+      } else {
+        console.log('no object');
+        setGenreFilters(mapOver([], currentGenreFilters));
+        setTimeFilters(mapOver([], currentTimeFilters));
       }
       return response.data.filters;
     } catch (err) {
@@ -49,6 +90,7 @@ export const FilterPage = ({ toggle, seeFilters, hidden }) => {
     }
   };
 
+  //converts state filter objects into two strings and saves them in the DB
   const clickOkay = async () => {
     try {
       const response = await axios({
@@ -56,7 +98,10 @@ export const FilterPage = ({ toggle, seeFilters, hidden }) => {
         withCredentials: true,
         url: `${serverURL}/likeTracker/filters`,
         data: {
-          filters: { genreFilters: genreFilters, timeFilters: timeFilters },
+          filters: {
+            genreFilters: reverseMapOver(genreFilters),
+            timeFilters: reverseMapOver(timeFilters),
+          },
         },
       });
       console.log(response.data);
