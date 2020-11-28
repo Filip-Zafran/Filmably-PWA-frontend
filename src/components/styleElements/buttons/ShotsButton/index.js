6268a807297aae2044ec636f120413306333d625
LocalMagic <amurray@zalando-62206.corp.ad.zalando.net>
2020-11-29 00:52:02 +0100

    added styled-components refactored shotsButton

--- src/components/styleElements/buttons/ShotsButton/index.js
+++ src/components/styleElements/buttons/ShotsButton/index.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import { Check, X } from '../../icons';
+import styled from "styled-components";
 import './ShotsButton.css';
+
+const ShotButton = styled.div`
+  width: 92px;
+  height: 92px;
+  cursor: pointer;
+`;
+
+const ShotButtonBorder = styled.div`
+  border-radius: 50%;
+  width: 92px;
+  height: 92px;
+
+  transform: ${props => props.active && 'scale(1.33)'};
+  transition: ${props => props.active && 'transform 0.3s linear'};
+  background: ${props => props.like ? 'var(--success-500-25)' : 'var(--error-500-25)'} ;
+`;
+
+const ShotButtonCenter = styled.div`
+  top: -81px;
+  left: 11px;
+  pointer-events: none;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+
+  background: ${props => (props.like && props.inactive) ? 'var(--success-500-25)' : props.like ? 'var(--success-500)' : props.inactive ? "var(--error-500-25)" : 'var(--error-500)'} ;
+`;
 
 export const ShotsButton = ({
   like,
@@ -10,37 +42,18 @@ export const ShotsButton = ({
   otherClickHandler,
 }) => {
   return (
-    <div className="shotsButton">
-      <div
+    <ShotButton >
+      <ShotButtonBorder like={like} active={active}
         onTouchStart={like ? clickHandler('like') : clickHandler('dislike')}
         onMouseDown={like ? clickHandler('like') : clickHandler('dislike')}
         onTouchEnd={clickHandler('neutral')}
         onMouseUp={
           like ? otherClickHandler('like') : otherClickHandler('dislike')
         }
-        className={`shotsButton__boarder ${
-          like && active
-            ? 'shotsButBoarderLike__Active'
-            : like
-            ? 'shotsButBoarderLike'
-            : active
-            ? 'shotsButBoarderDislike__Active'
-            : 'shotsButBoarderDislike'
-        }`}
-      ></div>
-      <div
-        className={`shotsButton__center ${
-          like && inactive
-            ? 'likeInactive'
-            : inactive
-            ? 'dislikeInactive'
-            : like
-            ? 'shotsButtonLike'
-            : 'shotsButtonDislike'
-        }`}
-      >
+      />
+      <ShotButtonCenter like={like} inactive={inactive}>
         {like ? <Check /> : <X />}
-      </div>
-    </div>
+      </ShotButtonCenter>
+    </ShotButton>
   );
 };
